f2b918de0ee03fbff75a5f51c1610c00a5ae4113
danaabdulhaq <danarabdulhaq@gmail.com>
2024-12-13 22:31:44 +0200

    Admin
    Admin For employee

---
+++ src/main/java/ps/project/resturantgroup8/UpdateEmployeeSuccessfullyController.java
@@ -0,0 +1,11 @@
+package ps.project.resturantgroup8;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class UpdateEmployeeSuccessfullyController {
+
+    @FXML
+    private TextField update_done;
+
+}
